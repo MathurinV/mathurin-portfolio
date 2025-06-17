6f7006595897fcce087d1d76c733a496378ca957
mathugoat <mathurin.valtel@supinfo.com>
2025-06-17 21:16:14 +0200

    Now self-hosting fonts 😎

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { repositoryName } from '$lib/prismicio';
 	import { PrismicPreview } from '@prismicio/svelte/kit';
+	import '@fontsource-variable/roboto';
+	import '@fontsource-variable/jetbrains-mono';
 	import '../app.css';
 	let { children } = $props();
 </script>
