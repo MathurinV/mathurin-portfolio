a9b2da2b81bc844a09b4903c38577dee9a91df01
MathurinV <122125621+MathurinV@users.noreply.github.com>
2025-06-17 13:54:21 +0200

    Added vercel analytics, /❌ feature has yet to be confirmed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	import '../app.css';
-	import { PrismicPreview } from '@prismicio/svelte/kit';
 	import { repositoryName } from '$lib/prismicio';
+	import { PrismicPreview } from '@prismicio/svelte/kit';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { onMount } from 'svelte';
+	import '../app.css';
 
 	let { children } = $props();
+	onMount(() => injectAnalytics());
 </script>
 
 <main>{@render children()}</main>
