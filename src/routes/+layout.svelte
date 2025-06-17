c51a800540c158237db89c882768a979b4092b15
MathurinV <122125621+MathurinV@users.noreply.github.com>
2025-06-17 14:22:26 +0200

    Final update on vercel's analytics -> right way to do it is to create +page.ts file and put the documentation content inside

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-	import { dev } from '$app/environment';
 	import { repositoryName } from '$lib/prismicio';
 	import { PrismicPreview } from '@prismicio/svelte/kit';
-	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	import '../app.css';
-
-	injectAnalytics({ mode: dev ? 'development' : 'production' });
 	let { children } = $props();
 </script>
 
