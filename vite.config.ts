9216d0b5afaf5474872a76c9cd60967cf75ad1e9
mathugoat <mathurin.valtel@supinfo.com>
2025-06-17 21:11:28 +0200

    Added qr codes  too hosted dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
+import { qrcode } from 'vite-plugin-qrcode';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit(), qrcode()],
 	server: {
 		fs: {
 			allow: ['./slicemachine.config.json']
