e231f80df5786b89fd9927bf6f58dc78600d5576
MathurinV <122125621+MathurinV@users.noreply.github.com>
2025-06-17 12:09:05 +0200

    Added CarouselCard component

---
+++ src/lib/components/CarouselCard/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { Content } from '@prismicio/client';
+	import { PrismicLink, PrismicRichText } from '@prismicio/svelte';
+	import type { Simplify } from '../../../prismicio-types';
+
+	const carousel_item: Simplify<Content.CarouselSliceDefaultPrimaryCarouselGroupItem> = $props();
+</script>
+
+<div class="flex flex-col items-center gap-1 px-2">
+	<PrismicLink field={carousel_item.media_link} class="font-mono hover:underline" />
+	<div class="relative">
+		<img
+			src={carousel_item.image.url}
+			alt={carousel_item.image.alt}
+			width="256"
+			class="svg-primary"
+		/>
+		{#if carousel_item.description && carousel_item.description.length > 0}
+			<div class="absolute inset-0 z-10 flex items-center justify-center p-4">
+				<div class="bg-surface-50-950/80 rounded p-2">
+					<PrismicRichText field={carousel_item.description} />
+				</div>
+			</div>
+		{/if}
+	</div>
+</div>
