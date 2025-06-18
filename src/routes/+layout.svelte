7506b8b737489a5a6f0be20cd4afbb1078d32f13
mathugoat <mathurin.valtel@supinfo.com>
2025-06-18 06:09:17 +0200

    Added speed insights to +layout.svelte, removed it from +layout.ts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import { repositoryName } from '$lib/prismicio';
 	import { PrismicPreview } from '@prismicio/svelte/kit';
 	import '@fontsource-variable/roboto';
 	import '@fontsource-variable/jetbrains-mono';
 	import '../app.css';
 	let { children } = $props();
+
+	injectSpeedInsights();
 </script>
 
 <main>{@render children()}</main>
